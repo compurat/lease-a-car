da6fa3351d34478a35da990e725dc5990cc333f4
compurat <compurat@gmail.com>
2025-08-04 16:01:56 +0200

    removed unused import

--- src/main/java/com/lease/car/config/SecurityUrlExcluder.java
+++ src/main/java/com/lease/car/config/SecurityUrlExcluder.java
@@ -1,10 +1,10 @@
 package com.lease.car.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.firewall.DefaultHttpFirewall;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
 
